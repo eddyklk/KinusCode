8afae7245945c7cf0fd53eaf7e38f3fcea2da125
Eddy junior valerio G <eddytaiment@gmail.com>
2022-10-12 23:55:32 -0400

    VARIABLES CREADAS.

--- KinusCode/Program.cs
+++ KinusCode/Program.cs
@@ -1,8 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-double No1, No2, No3, No4, No5, No6, No7 , No8 , No9 , No10 ;
-
+int No1, No2, No3, No4, No5, No6, No7 , No8 , No9 , No10, NumG;
+int num1 = 1, num2 = 2, num3 = 3, num4 = 4, num5 = 5, num6 = 6, num7 = 7, num8 = 8, num9 = 9, num10 = 10,
+    num11 = 11, num12 = 12, num13 = 13, num14 = 14, num15 = 15, num16 = 16, num17 = 17, num18 = 18, num19 = 19, num20 = 20,
+    num21 = 21, num22 = 22, num23 = 23, num24 = 24, num25 = 25, num26 = 26, num27 = 27, num28 = 28, num29 = 29, num30 = 30,
+    num31 = 31, num32 = 32, num33 = 33, num34 = 34, num35 = 35, num36 = 36, num37 = 37, num38 = 38, num39 = 39, num40 = 40,
+    num41 = 41, num42 = 42, num43 = 43, num44 = 44, num45 = 45, num46 = 46, num47 = 47, num48 = 48, num49 = 49, num50 = 50,
+    num51 = 51, num52 = 52, num53 = 53, num54 = 54, num55 = 55, num56 = 56, num57 = 57, num58 = 58, num59 = 59, num60 = 60,
+    num61 = 61, num62 = 62, num63 = 63, num64 = 64, num65 = 65, num66 = 66, num67 = 67, num68 = 68, num69 = 69, num70 = 70,
+    num71 = 71, num72 = 72, num73 = 73, num74 = 74, num75 = 75, num76 = 76, num77 = 77, num78 = 78;
 //for para generar los numero del 1 al 78.
 //comparar los numero dado con los que no.
 
@@ -32,3 +39,8 @@ Console.WriteLine("Digite el numero 10");
 No10 = Convert.ToInt32(Console.ReadLine());
 Console.ReadKey();
 
+
+for (int i = 0; i < NumG; i++))
+ {
+    
+}
