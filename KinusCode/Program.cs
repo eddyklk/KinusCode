dcbfd54288e220ce46c59685c6ff8f70c00ee312
Eddy junior valerio G <eddytaiment@gmail.com>
2022-10-12 23:05:22 -0400

    pide los numeros.

--- KinusCode/Program.cs
+++ KinusCode/Program.cs
@@ -1,12 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-int No1 = 0, No2 = 0, No3 = 0, No4 = 0, No5 = 0, No6 = 0, No7 = 0, No8 = 0, No9 = 0, No10 = 0;
+double No1, No2, No3, No4, No5, No6, No7 , No8 , No9 , No10 ;
 
 //for para generar los numero del 1 al 78.
 //comparar los numero dado con los que no.
 
 Console.WriteLine("Bienvenido a Kinus.");
 Console.WriteLine("Digite una jugada");
-Console.Read();
-No1 = ConvertTo(Console.ReadLine());
+Console.ReadKey();
+
+Console.WriteLine("Digite el numero 1");
+No1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite el numero 2");
+No2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite el numero 3");
+No3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite el numero 4");
+No4 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite el numero 5");
+No5 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite el numero 6");
+No6 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite el numero 7");
+No7 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite el numero 8");
+No8 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite el numero 9");
+No9 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite el numero 10");
+No10 = Convert.ToInt32(Console.ReadLine());
+Console.ReadKey();
+
