0a8385d703e71796980d1b36f1b147ebbccd4c5a
Eddy junior valerio G <eddytaiment@gmail.com>
2022-10-12 22:44:40 -0400

    Add project files.

---
+++ KinusCode/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int No1 = 0, No2 = 0, No3 = 0, No4 = 0, No5 = 0, No6 = 0, No7 = 0, No8 = 0, No9 = 0, No10 = 0;
+
+//for para generar los numero del 1 al 78.
+//comparar los numero dado con los que no.
+
+Console.WriteLine("Bienvenido a Kinus.");
+Console.WriteLine("Digite una jugada");
+Console.Read();
+No1 = ConvertTo(Console.ReadLine());
