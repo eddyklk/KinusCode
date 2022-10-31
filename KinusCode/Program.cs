bd65efcda816213dafa0c50d7ee01ff84b192a2e
Eddy Junior Valerio Gerardino <eddy.valerio@nordestana.com>
2022-10-31 17:46:04 -0400

    revisarlo

--- KinusCode/Program.cs
+++ KinusCode/Program.cs
@@ -177,8 +177,8 @@ foreach (var numero in lista)
     Console.ForegroundColor = ConsoleColor.Green;
     Console.WriteLine("Numero que se repiten.");
 
-int[] valores = new[]
-{   r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,
+int[] valores = new[]{
+    r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,
     r11,r12,r13,r14,r15,r16,r17,r18,r19,r20,
     r21,r22,r23,r24,r25,r26,r27,r28,r29,r30,
     r31,r32,r33,r34,r35,r36,r37,r38,r39,r40 };
@@ -198,7 +198,11 @@ int[] valores = new[]
     Console.WriteLine();
 
 
-    List<int> lista2 = new List<int> { 2, 5, 6, 6, 11, 1, 1, 2, 3, 3 };
+    List<int> lista2 = new List<int> { 
+    r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,
+    r11,r12,r13,r14,r15,r16,r17,r18,r19,r20,
+    r21,r22,r23,r24,r25,r26,r27,r28,r29,r30,
+    r31,r32,r33,r34,r35,r36,r37,r38,r39,r40 };
 
     var listaDistinct = lista2.Distinct();
 
