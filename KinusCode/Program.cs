97742dd56d740b26f5e36382915bc931712d084c
Eddy Junior Valerio Gerardino <eddy.valerio@nordestana.com>
2022-11-03 17:10:53 -0400

    Aparentemente terminado.

--- KinusCode/Program.cs
+++ KinusCode/Program.cs
@@ -1,4 +1,13 @@
 ﻿
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("Bienvenido a Kinus intento hacerte");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write(" MILLONARIO");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine(" :)");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Ahora ");
+
 
 //jugada 1
 int r1 = 0, r2 = 0, r3 = 0, r4 = 0, r5 = 0, r6 = 0, r7 = 0, r8 = 0, r9 = 0, r10 = 0,
@@ -20,91 +29,171 @@ Console.ForegroundColor = ConsoleColor.White;
 do
 {
     Console.WriteLine("Digite el primer numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r1 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el segundo numero entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r2 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el tercer numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r3 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el cuarto numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r4 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el quinto numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r5 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el sexto numero   entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r6 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el septimo numero entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r7 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el octavo numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r8 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el noveno numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r9 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el decimo numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r10 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
 
     //jugada 2
     Console.WriteLine("Digite el decimoprimer numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r11 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el decimosegundo numero entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r12 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el decimotercer numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r13 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el decimocuarto numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r14 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el decimoquinto numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r15 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el decimosexto numero   entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r16 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el decimoseptimo numero entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r17 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el decimoctavo numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r18 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el decimonoveno numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r19 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el vigésimo numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r20 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
 
     //jugada 3
     Console.WriteLine("Digite el vigésimoprimer numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r21 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el vigésimosegundo numero entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r22 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el vigésimotercer numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r23 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el vigésimocuarto numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r24 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el vigésimoquinto numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r25 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el vigésimosexto numero   entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r26 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el vigésimoseptimo numero entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r27 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el vigésimoctavo numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r28 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el vigésimonoveno numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r29 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el trigésimo numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r30 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
 
     //jugada 4
     Console.WriteLine("Digite el trigesimoprimer numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r31 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el trigesimosegundo numero entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r32 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el trigesimotercer numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r33 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el trigesimocuarto numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r34 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el trigesimoquinto numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r35 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el trigesimosexto numero   entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r36 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el trigesimoseptimo numero entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r37 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el trigesimoctavo numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r38 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el trigesimonoveno numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r39 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine("Digite el Cuadragésimo numero  entre el 1 y el 80");
+    Console.ForegroundColor = ConsoleColor.Yellow;
     r40 = Convert.ToInt32(Console.ReadLine());
+    Console.ForegroundColor = ConsoleColor.White;
 
 }
 while (
@@ -120,14 +209,14 @@ while (
 (r10 < 1 || r10 > 80)
 );
 
-Console.ReadKey();
+Console.WriteLine();
 
 
 Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine("Quieres registrar los numeros que salieron?. ");
 Console.WriteLine("Escriba True en caso si o escriba false en caso de que no. ");
+Console.ForegroundColor = ConsoleColor.Yellow;
 jugada = Convert.ToBoolean( Console.ReadLine());
-Console.ReadKey();
 
 
 Console.ForegroundColor = ConsoleColor.White;
@@ -136,48 +225,90 @@ do
 {
     do
     {
+        
         Console.WriteLine("Digite el primer numero  entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s1 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Digite el segundo numero entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s2 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Digite el tercer numero  entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s3 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Digite el cuarto numero  entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s4 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Digite el quinto numero  entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s5 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Digite el sexto numero   entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s6 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Digite el septimo numero entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s7 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Digite el octavo numero  entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s8 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Digite el noveno numero  entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s9 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Digite el decimo numero  entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s10 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
 
         //jugada 2
         Console.WriteLine("Digite el decimoprimer numero  entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s11 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Digite el decimosegundo numero entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s12 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Digite el decimotercer numero  entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s13 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Digite el decimocuarto numero  entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s14 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Digite el decimoquinto numero  entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s15 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Digite el decimosexto numero   entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s16 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Digite el decimoseptimo numero entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s17 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Digite el decimoctavo numero  entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s18 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Digite el decimonoveno numero  entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s19 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Digite el vigésimo numero  entre el 1 y el 20");
+        Console.ForegroundColor = ConsoleColor.Yellow;
         s20 = Convert.ToInt32(Console.ReadLine());
+        Console.ForegroundColor = ConsoleColor.White;
+        jugada = false;
     }
     while (
         (r1 < 1 || r1 > 20) &&
@@ -206,6 +337,7 @@ while (jugada == true)
 //(r9 < 1 || r9 > 80) &&
 //(r10 < 1 || r10 > 80)
 ;
+
 
 Console.WriteLine();
 Console.ForegroundColor = ConsoleColor.Green;
@@ -256,7 +388,7 @@ List<int> lista = new List<int>()
 
 foreach (var numero in lista)
     {
-    Console.ForegroundColor = ConsoleColor.Red;
+    Console.ForegroundColor = ConsoleColor.Yellow;
     Console.Write(numero + ",");
     }
 
@@ -274,7 +406,7 @@ int[] valores = new[]{
 
     foreach (var grouping in valores.GroupBy(t => t).Where(t => t.Count() != 1))
     {
-        Console.ForegroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine(string.Format("'{0}' está repetido {1} veces.", grouping.Key, grouping.Count()));
     }
 
@@ -298,7 +430,7 @@ int[] valores = new[]{
     Console.Write("Numero que son unicos: ");
     foreach (int num in listaDistinct)
     {
-        Console.ForegroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.Yellow;
         Console.Write(num + ",");
 
     }
@@ -318,26 +450,30 @@ int[] valores = new[]{
 
     Console.WriteLine(random);
 
-
+    Console.WriteLine();
     Console.ForegroundColor = ConsoleColor.Green;
     Console.Write("Numero Jugados: ");
 
-    Console.ForegroundColor = ConsoleColor.Red;
+    Console.ForegroundColor = ConsoleColor.Yellow;
     Console.Write(r1 + " " + r2 + " " + r3 + " " + r4 + " " + r5 + " " + r6 + " " + r7 + " " + r8 + " " + r9 + " " + r10 + " " +
     r11 + " " + r12 + " " + r13 + " " + r14 + " " + r15 + " " + r16 + " " + r17 + " " + r18 + " " + r19 + " " + r20 + " " +
     r21 + " " + r22 + " " + r23 + " " + r24 + " " + r25 + " " + r26 + " " + r27 + " " + r28 + " " + r29 + " " + r30 + " " +
     r31 + " " + r32 + " " + r33 + " " + r34 + " " + r35 + " " + r36 + " " + r37 + " " + r38 + " " + r39 + " " + r40);
 
+
+Console.WriteLine();
+jugada = true;
 if (jugada == true)
 {
     Console.ForegroundColor = ConsoleColor.Green;
     Console.Write("Numero que salieron: ");
 
-    Console.ForegroundColor = ConsoleColor.Red;
+    Console.ForegroundColor = ConsoleColor.Yellow;
     Console.Write(s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5 + " " + s6 + " " + s7 + " " + s8 + " " + s9 + " " + s10 + " " +
         s11 + " " + s12 + " " + s13 + " " + s14 + " " + s15 + " " + s16 + " " + s17 + " " + s18 + " " + s19 + " " + s20);
 }
 
+Console.WriteLine();
 Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine("Numeros que coinciden que tu jugada: ");
 
@@ -350,7 +486,7 @@ int[] coinciden = new[]{
 
 foreach (var grouping in coinciden.GroupBy(t => t).Where(t => t.Count() != 1))
 {
-    Console.ForegroundColor = ConsoleColor.Red;
+    Console.ForegroundColor = ConsoleColor.Yellow;
     Console.WriteLine(string.Format("'{0}' está repetido {1} veces.", grouping.Key, grouping.Count()));
 }
 
